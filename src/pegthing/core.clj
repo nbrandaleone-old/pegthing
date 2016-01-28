ddf0b96bef8d1526c422009b097ad452aaeacb2a
Nick Brandaleone <nbrand@mac.com>
2016-01-28 14:40:43 -0500

    initial commit
    Program from “Clojure for the brave and true”.

---
+++ src/pegthing/core.clj
@@ -0,0 +1,51 @@
+(ns pegthing.core
+  (require [clojure.set :as set])
+  (:gen-class))
+
+(declare successful-move prompt-mpve game-over query-rows)
+
+(defn tri*
+  "Generates lazy sequence of triangular numbers"
+  ([] (tri* 0 1))
+  ([sum n]
+    (let [new-sum (+ sum n)]
+      (cons new-sum (lazy-seq (tri* new-sum (inc n)))))))
+      
+(def tri (tri*))
+
+(defn  triangular?
+  "Is the number triangular? e.g. 1, 3, 6, 10, 15, etc"
+  [n]
+  (= n (last (take-while #(>= n %) tri))))
+
+(defn row-tri
+  "The triangular number at the end of row n"
+  [n]
+  (last (take n tri)))
+
+(row-tri 3)
+
+(defn row-num
+  "Returns row number the position belongs to: pos 1 in row 1,
+  positions 2 and 3 in row 2, etc"
+  [pos]
+  (inc (count (take-while #(> pos %) tri))))
+
+(row-num 5)
+
+(defn connect
+  "Form a mutual connection between two positions"
+  [board max-pos pos neighbor destination]
+  (if (<= destination max-pos)
+    (reduce (fn [new-board [p1 p2]]
+              (assoc-in new-board [p1 :connections p2] neighbor))
+            board
+	    [[pos destination] [destination pos]])
+    board))
+
+(connect {} 15 1 2 4)
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
